3cbbd135caae2a8c62784a56546ca6bc21f62249
Barrospaulo <pafbarros.sx@gmail.com>
2025-07-01 16:07:39 +0100

    .

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ app.post('/api/servicos', authenticateToken, async (req, res, next) => {
     console.log('Dados recebidos:', req.body);
 
     const {
-      data_servico, hora_servico, status, autorServico, clienteId, // Adicione clienteId aqui
+      dataServico, horaServico, status, autorServico, clienteId, // Adicione clienteId aqui
       nomeCliente, telefoneContato, marcaAparelho, modeloAparelho, 
       problemaCliente, solucaoInicial, valorTotal, observacoes
     } = req.body;
